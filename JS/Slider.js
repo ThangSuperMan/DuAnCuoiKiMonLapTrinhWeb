00f10ff220e2b0ece167d520432b1ff9dd48fbbb
Phan Thắng <phanthang@Phans-MacBook-Air.local>
2021-10-30 17:04:36 +0700

    first commit

---
+++ JS/Slider.js
@@ -0,0 +1,24 @@
+const sliderImage = document.querySelector('.slider-item-image');
+console.log(sliderImage)
+
+
+// Variables
+let index = 0;
+const images = ['../images/slider1.jpeg', '../images/slider2.jpeg', '../images/slider3.jpeg', '../images/slider4.jpeg', '../images/slider5.jpeg']
+
+// Functions
+function handleChangeImageSlider() {
+
+    if (index == images.length) {
+        console.log('Max image');
+        // Reset the first image 
+        index = 0;
+    }
+
+    sliderImage.style.backgroundImage = `url('${images[index]}')`;
+    
+    // Update index
+    index++;
+}
+
+setInterval(handleChangeImageSlider, 7000);
