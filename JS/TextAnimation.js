bb6bc3b02865f115664e2388465a32f073bc06ea
Phan Thắng <phanthang@Phans-MacBook-Air.local>
2021-10-31 16:55:32 +0700

    thirh commit

---
+++ JS/TextAnimation.js
@@ -0,0 +1,22 @@
+const textDestination = document.querySelector('.destinate-vietnam');
+
+const word = textDestination.innerText.split('');
+
+// Reset value of textDestination
+textDestination.children[0].innerHTML = '';
+console.log(textDestination.children[0])
+
+word.forEach((letter, index) => {
+    textDestination.children[0].innerHTML += `<span style="--index: ${index};">${letter}</span>`
+})
+
+// Clone div
+let cloneDiv = textDestination.children[0].cloneNode(true);
+cloneDiv.style.position = 'absolute';
+cloneDiv.style.top = '0';
+cloneDiv.style.left = '0';
+cloneDiv.style.zIndex = '4';
+
+
+// Append child
+textDestination.appendChild(cloneDiv);
